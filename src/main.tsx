0dfe9b47f7d8f40345a78f46b5732c7a0194b1d9
Camila Martinez <camilamarb98@gmail.com>
2023-03-26 17:16:21 +0200

    Add ReactRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouteObject,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./layouts/Root";
+import Error_404 from "./pages/404";
+import Account from "./pages/Account";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "account",
+        element: <Account />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "*",
+        element: <Error_404 />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={router} />
+);
